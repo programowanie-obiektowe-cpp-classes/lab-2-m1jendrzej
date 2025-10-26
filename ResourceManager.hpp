5d2f4e90e65c849430caf3a2ace081eb96482ac7
m1jendrzej <mjendrzej03@gmail.com>
2025-10-26 11:41:17 +0100

    Zad 1 oraz Zad 2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -2,7 +2,29 @@
 
 #include "Resource.hpp"
 
+
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+private:
+    Resource* res; 
+
+public:
+    ResourceManager() : res(new Resource()) {}
+
+    ~ResourceManager() { delete res; }
+
+    ResourceManager(const ResourceManager& other) : res(new Resource(*other.res)) {}
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        if (this != &other) {
+            ResourceManager temp(other); 
+            std::swap(res, temp.res);    
+        }
+        return *this;
+    }
+
+
+    double get() const { return res->get(); }
 };
+
